55c93b0ada2b665555d33d67167fb2766edd8fc0
Cormac Relf <cormacrelf@me.com>
2017-11-24 12:39:28 +1100

    resolve typescript properly + put file watcher behind a flag

--- typedoc.sh
+++ typedoc.sh
@@ -1,1 +1,11 @@
-rg --files docs packages/angular-hovercraft/src | entr npm run typedoc -- --out out-docs --exclude '**/{internal,utils,node_modules}/**/*' --mode modules --options typedoc.json --theme packages/custom-typedoc-theme/bin/default --includes ./docs --readme ./README.md ./packages/angular-hovercraft/src/
+RUN_ENTR=$1
+
+COMMAND="yarn run typedoc -- --out out-docs --exclude '**/{internal,utils,node_modules}/**/*' --mode modules --options typedoc.json --theme packages/custom-typedoc-theme/bin/default --includes ./docs --readme ./README.md ./packages/angular-hovercraft/src/"
+
+if [[ "$RUN_ENTR" == "--watch" ]]; then
+  rg --files docs packages/angular-hovercraft/src | entr $COMMAND
+else
+  $COMMAND
+fi
+
+
