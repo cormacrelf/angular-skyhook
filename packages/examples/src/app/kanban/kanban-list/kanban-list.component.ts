4adcadb8b780e925842fab305f474823550e35ca
Cormac Relf <cormacrelf@me.com>
2018-06-19 12:50:11 +1000

    forgot one DragSource -> DragSource<Item> file

--- packages/examples/src/app/kanban/kanban-list/kanban-list.component.ts
+++ packages/examples/src/app/kanban/kanban-list/kanban-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, EventEmitter } from "@angular/core";
 import { Card } from "../card";
 import { DragSource, SkyhookDndService } from "angular-skyhook";
-import { DropEvent } from "angular-skyhook-card-list";
+import { DropEvent, DraggedItem } from "angular-skyhook-card-list";
 import { Output } from "@angular/core";
 import { getEmptyImage } from "react-dnd-html5-backend";
 import { ItemTypes } from "../item-types";
@@ -13,7 +13,7 @@ import { ItemTypes } from "../item-types";
 })
 export class KanbanListComponent {
     @Input() list: { id: any, title: string, cards: Card[] };
-    @Input() source: DragSource;
+    @Input() source: DragSource<DraggedItem>;
     @Input() dragging = false;
     @Output() dropCard = new EventEmitter<DropEvent>();
     @Output() addCard = new EventEmitter<string>();
