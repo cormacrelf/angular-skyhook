1c531ef6392f197d2cb05a2eb5bc86d1c03449e0
Cormac Relf <cormacrelf@me.com>
2018-06-19 22:09:03 +1000

    code formatting

--- packages/angular-skyhook/src/dnd.module.ts
+++ packages/angular-skyhook/src/dnd.module.ts
@@ -4,37 +4,57 @@
  */
 /** a second comment */
 
-import { NgModule, ModuleWithProviders, InjectionToken, NgZone } from '@angular/core';
+import {
+    NgModule,
+    ModuleWithProviders,
+    InjectionToken,
+    NgZone
+} from '@angular/core';
 
 import { SkyhookDndService } from './connector.service';
-import { DragSourceDirective, DropTargetDirective, DragPreviewDirective } from './dnd.directive';
+import {
+    DragSourceDirective,
+    DropTargetDirective,
+    DragPreviewDirective
+} from './dnd.directive';
 
 import { DRAG_DROP_BACKEND, DRAG_DROP_MANAGER } from './tokens';
 
-import { DragDropManager, createDragDropManager, BackendFactory, Backend } from 'dnd-core';
+import {
+    DragDropManager,
+    createDragDropManager,
+    BackendFactory,
+    Backend
+} from 'dnd-core';
 
 import { invariant } from './internal/invariant';
 
 /** @ignore */
 export function unpackBackendForEs5Users(backendOrModule: any) {
-  // Auto-detect ES6 default export for people still using ES5
-  let backend = backendOrModule;
-  if (typeof backend === 'object' && typeof backend.default === 'function') {
-    backend = backend.default;
-  }
-  invariant(
-    typeof backend === 'function',
-    'Expected the backend to be a function or an ES6 module exporting a default function. ' +
-    'Read more: http://react-dnd.github.io/react-dnd/docs-drag-drop-context.html',
-  );
-  return backend;
+    // Auto-detect ES6 default export for people still using ES5
+    let backend = backendOrModule;
+    if (typeof backend === 'object' && typeof backend.default === 'function') {
+        backend = backend.default;
+    }
+    invariant(
+        typeof backend === 'function',
+        'Expected the backend to be a function or an ES6 module exporting a default function. ' +
+            'Read more: http://react-dnd.github.io/react-dnd/docs-drag-drop-context.html'
+    );
+    return backend;
 }
 
 // TODO allow injecting window
 /** @ignore */
-export function managerFactory(backendFactory: BackendFactory, zone: NgZone, context = { 'window': window }) {
-  backendFactory = unpackBackendForEs5Users(backendFactory);
-  return zone.runOutsideAngular(() => createDragDropManager(backendFactory, context));
+export function managerFactory(
+    backendFactory: BackendFactory,
+    zone: NgZone,
+    context = { window: window }
+) {
+    backendFactory = unpackBackendForEs5Users(backendFactory);
+    return zone.runOutsideAngular(() =>
+        createDragDropManager(backendFactory, context)
+    );
 }
 
 /*
@@ -75,39 +95,36 @@ export interface BackendFactoryInput {
 }
 
 @NgModule({
-  imports: [
-  ],
-  declarations: [
-    DragSourceDirective,
-    DropTargetDirective,
-    DragPreviewDirective,
-  ],
-  providers: [],
-  exports: [
-    DragSourceDirective,
-    DropTargetDirective,
-    DragPreviewDirective,
-  ]
+    imports: [],
+    declarations: [
+        DragSourceDirective,
+        DropTargetDirective,
+        DragPreviewDirective
+    ],
+    providers: [],
+    exports: [DragSourceDirective, DropTargetDirective, DragPreviewDirective]
 })
 export class SkyhookDndModule {
-  static forRoot(backendOrBackendFactory: BackendInput | BackendFactoryInput): ModuleWithProviders {
-    return {
-      ngModule: SkyhookDndModule,
-      providers: [
-        {
-          provide: DRAG_DROP_BACKEND,
-          // whichever one they have provided, the other will be undefined
-          useValue: (backendOrBackendFactory as BackendInput).backend,
-          useFactory: (backendOrBackendFactory as BackendFactoryInput).backendFactory
-        },
-        {
-          provide: DRAG_DROP_MANAGER,
-          useFactory: managerFactory,
-          deps: [DRAG_DROP_BACKEND, NgZone]
-        },
-        SkyhookDndService,
-      ]
-    };
-  }
-
+    static forRoot(
+        backendOrBackendFactory: BackendInput | BackendFactoryInput
+    ): ModuleWithProviders {
+        return {
+            ngModule: SkyhookDndModule,
+            providers: [
+                {
+                    provide: DRAG_DROP_BACKEND,
+                    // whichever one they have provided, the other will be undefined
+                    useValue: (backendOrBackendFactory as BackendInput).backend,
+                    useFactory: (backendOrBackendFactory as BackendFactoryInput)
+                        .backendFactory
+                },
+                {
+                    provide: DRAG_DROP_MANAGER,
+                    useFactory: managerFactory,
+                    deps: [DRAG_DROP_BACKEND, NgZone]
+                },
+                SkyhookDndService
+            ]
+        };
+    }
 }
--- packages/angular-skyhook/src/internal/connection-factory.ts
+++ packages/angular-skyhook/src/internal/connection-factory.ts
@@ -7,7 +7,13 @@ import { Backend, DragDropManager } from 'dnd-core';
 import { NgZone } from '@angular/core';
 import { invariant } from './invariant';
 import { TypeOrTypeArray } from '../type-ish';
-import { Subscription, Observable, ReplaySubject, BehaviorSubject, TeardownLogic } from 'rxjs';
+import {
+    Subscription,
+    Observable,
+    ReplaySubject,
+    BehaviorSubject,
+    TeardownLogic
+} from 'rxjs';
 import { TYPE_DYNAMIC } from '../tokens';
 
 import { take, map, distinctUntilChanged, switchMapTo } from 'rxjs/operators';
@@ -17,216 +23,251 @@ import { areCollectsEqual } from '../utils/areCollectsEqual';
 import { DropTargetMonitor } from '../target-monitor';
 import { DragSourceMonitor } from '../source-monitor';
 import * as t from '../connection-types';
-import { DropTargetConnector, DragSourceConnector, DragSourceOptions, DragPreviewOptions } from '../connectors';
+import {
+    DropTargetConnector,
+    DragSourceConnector,
+    DragSourceOptions,
+    DragPreviewOptions
+} from '../connectors';
 import { scheduleMicroTaskAfter } from './scheduleMicroTaskAfter';
 
 export interface FactoryArgs<TMonitor, TConnector> {
-  createHandler: (handlerMonitor: any) => any;
-  createMonitor: (manager: DragDropManager<any>) => TMonitor;
-  createConnector: (backend: Backend) => { receiveHandlerId(handlerId: any): void; hooks: TConnector; };
-  registerHandler: (type: any, handler: any, manager: DragDropManager<any>) => {
-    handlerId: any;
-    unregister: Subscription | Function;
-  };
+    createHandler: (handlerMonitor: any) => any;
+    createMonitor: (manager: DragDropManager<any>) => TMonitor;
+    createConnector: (
+        backend: Backend
+    ) => { receiveHandlerId(handlerId: any): void; hooks: TConnector };
+    registerHandler: (
+        type: any,
+        handler: any,
+        manager: DragDropManager<any>
+    ) => {
+        handlerId: any;
+        unregister: Subscription | Function;
+    };
 }
 
 export interface SourceConstructor {
-  new(manager: any, ngZone: NgZone, skyhookZone: Zone, initialType: string | symbol | undefined): t.DragSource<any>;
+    new (
+        manager: any,
+        ngZone: NgZone,
+        skyhookZone: Zone,
+        initialType: string | symbol | undefined
+    ): t.DragSource<any>;
 }
 export interface TargetConstructor {
-  new(manager: any, ngZone: NgZone, skyhookZone: Zone, initialType: TypeOrTypeArray | undefined): t.DropTarget;
+    new (
+        manager: any,
+        ngZone: NgZone,
+        skyhookZone: Zone,
+        initialType: TypeOrTypeArray | undefined
+    ): t.DropTarget;
 }
 
 export function sourceConnectionFactory(
-  factoryArgs: FactoryArgs<DragSourceMonitor,
-    DragSourceConnector>
+    factoryArgs: FactoryArgs<DragSourceMonitor, DragSourceConnector>
 ): SourceConstructor {
-  return connectionFactory(factoryArgs) as SourceConstructor;
+    return connectionFactory(factoryArgs) as SourceConstructor;
 }
 
 export function targetConnectionFactory(
-  factoryArgs: FactoryArgs<DropTargetMonitor,
-    DropTargetConnector>
+    factoryArgs: FactoryArgs<DropTargetMonitor, DropTargetConnector>
 ): TargetConstructor {
-  return connectionFactory(factoryArgs) as TargetConstructor;
+    return connectionFactory(factoryArgs) as TargetConstructor;
 }
 
-function connectionFactory<TMonitor extends DragSourceMonitor | DropTargetMonitor, TConnector>(
-  factoryArgs: FactoryArgs<TMonitor, TConnector>
-) {
+function connectionFactory<
+    TMonitor extends DragSourceMonitor | DropTargetMonitor,
+    TConnector
+>(factoryArgs: FactoryArgs<TMonitor, TConnector>) {
+    class ConnectionInner<T> {
+        // immutable after instantiation
+        private readonly handlerMonitor: any;
+        private readonly handlerConnector: any & { hooks: any };
+        private readonly handler: any;
 
-  class ConnectionInner<T> {
+        /** The stream of all change events from the internal subscription's handleChange */
+        private readonly collector$: BehaviorSubject<TMonitor>;
+        /** A subject basically used to kick off any observables waiting for a type to be set via setType/setTypes */
+        private readonly resolvedType$ = new ReplaySubject<any>(1);
 
-    // immutable after instantiation
-    private readonly handlerMonitor: any;
-    private readonly handlerConnector: any & { hooks: any };
-    private readonly handler: any;
+        // mutable state
+        private currentType: TypeOrTypeArray;
+        private handlerId: any;
 
-    /** The stream of all change events from the internal subscription's handleChange */
-    private readonly collector$: BehaviorSubject<TMonitor>;
-    /** A subject basically used to kick off any observables waiting for a type to be set via setType/setTypes */
-    private readonly resolvedType$ = new ReplaySubject<any>(1);
+        /**
+         * This one is created and destroyed once per type or list of types.
+         * Because each time we change the type, we unsubscribe from the global state storage and
+         * re-subscribe with the new type.
+         */
+        private subscriptionTypeLifetime: Subscription;
 
-    // mutable state
-    private currentType: TypeOrTypeArray;
-    private handlerId: any;
+        /**
+         * This one lives exactly as long as the connection.
+         * It is responsible for disposing of the handlerConnector, and any internal listen() subscriptions.
+         */
+        private subscriptionConnectionLifetime = new Subscription();
 
-    /**
-     * This one is created and destroyed once per type or list of types.
-     * Because each time we change the type, we unsubscribe from the global state storage and
-     * re-subscribe with the new type.
-     */
-    private subscriptionTypeLifetime: Subscription;
+        constructor(
+            private manager: DragDropManager<any>,
+            private ngZone: NgZone,
+            private skyhookZone: Zone,
+            initialType: TypeOrTypeArray | undefined
+        ) {
+            invariant(
+                typeof manager === 'object',
+                // TODO: update this mini-documentation
+                'Could not find the drag and drop manager in the context of %s. ' +
+                    'Make sure to wrap the top-level component of your app with DragDropContext. '
+                // tslint:disable-next-line:max-line-length
+                // 'Read more: ',
+            );
+            NgZone.assertNotInAngularZone();
 
-    /**
-     * This one lives exactly as long as the connection.
-     * It is responsible for disposing of the handlerConnector, and any internal listen() subscriptions.
-     */
-    private subscriptionConnectionLifetime = new Subscription();
+            this.handlerMonitor = factoryArgs.createMonitor(this.manager);
+            this.collector$ = new BehaviorSubject(this.handlerMonitor);
+            this.handler = factoryArgs.createHandler(this.handlerMonitor);
+            this.handlerConnector = factoryArgs.createConnector(
+                this.manager.getBackend()
+            );
+            // handlerConnector lives longer than any per-type subscription
+            this.subscriptionConnectionLifetime.add(() =>
+                this.handlerConnector.receiveHandlerId(null)
+            );
 
-    constructor(
-      private manager: DragDropManager<any>,
-      private ngZone: NgZone,
-      private skyhookZone: Zone,
-      initialType: TypeOrTypeArray | undefined,
-    ) {
-      invariant(
-        typeof manager === 'object',
-        // TODO: update this mini-documentation
-        'Could not find the drag and drop manager in the context of %s. ' +
-        'Make sure to wrap the top-level component of your app with DragDropContext. '
-        // tslint:disable-next-line:max-line-length
-        // 'Read more: ',
-      );
-      NgZone.assertNotInAngularZone();
+            if (initialType && initialType !== TYPE_DYNAMIC) {
+                this.setTypes(initialType);
+            }
+        }
 
-      this.handlerMonitor = factoryArgs.createMonitor(this.manager);
-      this.collector$ = new BehaviorSubject(this.handlerMonitor);
-      this.handler = factoryArgs.createHandler(this.handlerMonitor);
-      this.handlerConnector = factoryArgs.createConnector(this.manager.getBackend());
-      // handlerConnector lives longer than any per-type subscription
-      this.subscriptionConnectionLifetime.add(() => this.handlerConnector.receiveHandlerId(null));
+        listen<P>(mapFn: (monitor: TMonitor) => P): Observable<P> {
+            // Listeners are generally around as long as the connection.
+            // This isn't 100% true, but there is no way of knowing (even if you ref-count it)
+            // when a component no longer needs it.
+            return this.resolvedType$.pipe(
+                // this ensures we don't start emitting values until there is a type resolved
+                take(1),
+                // switch our attention to the incoming firehose of 'something changed' events
+                switchMapTo(this.collector$),
+                // turn them into 'interesting state' via the monitor and a user-provided function
+                map(mapFn),
+                // don't emit EVERY time the firehose says something changed, only when the interesting state changes
+                distinctUntilChanged(areCollectsEqual),
+                // this schedules a single batch change detection run after all the listeners have heard their newest value
+                // thus all changes resulting from subscriptions to this are caught by the
+                // change detector.
+                scheduleMicroTaskAfter(this.skyhookZone)
+            );
+        }
 
-      if (initialType && initialType !== TYPE_DYNAMIC) {
-        this.setTypes(initialType);
-      }
+        connect(fn: (connector: TConnector) => void): Subscription {
+            return this.resolvedType$.pipe(take(1)).subscribe(() => {
+                // must run inside skyhookZone, so things like timers firing after a long hover with touch backend
+                // will cause change detection (via executing a macro or event task)
+                this.skyhookZone.run(() => {
+                    fn(this.handlerConnector.hooks);
+                });
+            });
+        }
+
+        connectDropTarget(node: Node): Subscription {
+            return this.connect(c =>
+                ((c as any) as DropTargetConnector).dropTarget(node)
+            );
+        }
+
+        connectDragSource(
+            node: Node,
+            options: DragSourceOptions
+        ): Subscription {
+            return this.connect(c =>
+                ((c as any) as DragSourceConnector).dragSource(node, options)
+            );
+        }
+
+        connectDragPreview(
+            node: Node,
+            options: DragPreviewOptions
+        ): Subscription {
+            return this.connect(c =>
+                ((c as any) as DragSourceConnector).dragPreview(node, options)
+            );
+        }
+
+        setTypes(type: TypeOrTypeArray) {
+            // must run inside skyhookZone, so things like timers firing after a long hover with touch backend
+            // will cause change detection (via executing a macro or event task)
+            this.skyhookZone.run(() => {
+                this.receiveType(type);
+                this.resolvedType$.next(1);
+            });
+        }
+
+        setType(type: string | symbol) {
+            this.setTypes(type);
+        }
+
+        getHandlerId() {
+            return this.handlerId;
+        }
+
+        receiveType(type: TypeOrTypeArray) {
+            if (type === this.currentType) {
+                return;
+            }
+
+            NgZone.assertNotInAngularZone();
+
+            this.currentType = type;
+
+            if (this.subscriptionTypeLifetime) {
+                this.subscriptionTypeLifetime.unsubscribe();
+            }
+            // console.debug('subscribed to ' + type.toString());
+            this.subscriptionTypeLifetime = new Subscription();
+
+            const { handlerId, unregister } = factoryArgs.registerHandler(
+                type,
+                this.handler,
+                this.manager
+            );
+
+            this.handlerId = handlerId;
+            this.handlerMonitor.receiveHandlerId(handlerId);
+            this.handlerConnector.receiveHandlerId(handlerId);
+
+            const globalMonitor = this.manager.getMonitor();
+            const unsubscribe = globalMonitor.subscribeToStateChange(
+                this.handleChange,
+                { handlerIds: [handlerId] }
+            );
+
+            this.subscriptionTypeLifetime.add(unsubscribe);
+            this.subscriptionTypeLifetime.add(unregister);
+            // this.subscriptionTypeLifetime.add(() => console.debug("unsubscribed from " + type.toString()));
+        }
+
+        private handleChange = () => {
+            this.collector$.next(this.handlerMonitor);
+        };
+
+        unsubscribe() {
+            if (this.subscriptionTypeLifetime) {
+                this.subscriptionTypeLifetime.unsubscribe();
+            }
+            this.subscriptionConnectionLifetime.unsubscribe();
+        }
+
+        add(teardown: TeardownLogic): Subscription {
+            return this.subscriptionConnectionLifetime.add(teardown);
+        }
+
+        get closed() {
+            return (
+                this.subscriptionConnectionLifetime &&
+                this.subscriptionConnectionLifetime.closed
+            );
+        }
     }
-
-    listen<P>(mapFn: (monitor: TMonitor) => P): Observable<P> {
-
-      // Listeners are generally around as long as the connection.
-      // This isn't 100% true, but there is no way of knowing (even if you ref-count it)
-      // when a component no longer needs it.
-      return this.resolvedType$.pipe(
-        // this ensures we don't start emitting values until there is a type resolved
-        take(1),
-        // switch our attention to the incoming firehose of 'something changed' events
-        switchMapTo(this.collector$),
-        // turn them into 'interesting state' via the monitor and a user-provided function
-        map(mapFn),
-        // don't emit EVERY time the firehose says something changed, only when the interesting state changes
-        distinctUntilChanged(areCollectsEqual),
-        // this schedules a single batch change detection run after all the listeners have heard their newest value
-        // thus all changes resulting from subscriptions to this are caught by the
-        // change detector.
-        scheduleMicroTaskAfter(this.skyhookZone)
-      );
-    }
-
-    connect(fn: (connector: TConnector) => void): Subscription {
-      return this.resolvedType$.pipe(take(1)).subscribe(() => {
-        // must run inside skyhookZone, so things like timers firing after a long hover with touch backend
-        // will cause change detection (via executing a macro or event task)
-        this.skyhookZone.run(() => {
-          fn(this.handlerConnector.hooks);
-        });
-      });
-    }
-
-    connectDropTarget(node: Node): Subscription {
-      return this.connect(c => (c as any as DropTargetConnector).dropTarget(node));
-    }
-
-    connectDragSource(node: Node, options: DragSourceOptions): Subscription {
-      return this.connect(c => (c as any as DragSourceConnector).dragSource(node, options));
-    }
-
-    connectDragPreview(node: Node, options: DragPreviewOptions): Subscription {
-      return this.connect(c => (c as any as DragSourceConnector).dragPreview(node, options));
-    }
-
-    setTypes(type: TypeOrTypeArray) {
-      // must run inside skyhookZone, so things like timers firing after a long hover with touch backend
-      // will cause change detection (via executing a macro or event task)
-      this.skyhookZone.run(() => {
-        this.receiveType(type);
-        this.resolvedType$.next(1);
-      });
-    }
-
-    setType(type: string | symbol) {
-      this.setTypes(type);
-    }
-
-    /** Returns the drag source ID that can be used to simulate the drag and drop events with the testing backend. */
-    getHandlerId() {
-      return this.handlerId;
-    }
-
-    receiveType(type: TypeOrTypeArray) {
-      if (type === this.currentType) {
-        return;
-      }
-
-      NgZone.assertNotInAngularZone();
-
-      this.currentType = type;
-
-      if (this.subscriptionTypeLifetime) {
-        this.subscriptionTypeLifetime.unsubscribe();
-      }
-      // console.debug('subscribed to ' + type.toString());
-      this.subscriptionTypeLifetime = new Subscription();
-
-      const { handlerId, unregister } = factoryArgs.registerHandler(
-        type,
-        this.handler,
-        this.manager,
-      );
-
-      this.handlerId = handlerId;
-      this.handlerMonitor.receiveHandlerId(handlerId);
-      this.handlerConnector.receiveHandlerId(handlerId);
-
-      const globalMonitor = this.manager.getMonitor();
-      const unsubscribe = globalMonitor.subscribeToStateChange(
-        this.handleChange,
-        { handlerIds: [handlerId] },
-      );
-
-      this.subscriptionTypeLifetime.add(unsubscribe);
-      this.subscriptionTypeLifetime.add(unregister);
-      // this.subscriptionTypeLifetime.add(() => console.debug("unsubscribed from " + type.toString()));
-    }
-
-    private handleChange = () => {
-      this.collector$.next(this.handlerMonitor);
-    }
-
-    unsubscribe() {
-      if (this.subscriptionTypeLifetime) { this.subscriptionTypeLifetime.unsubscribe(); }
-      this.subscriptionConnectionLifetime.unsubscribe();
-    }
-
-    add(teardown: TeardownLogic): Subscription {
-      return this.subscriptionConnectionLifetime.add(teardown);
-    }
-
-    get closed() {
-      return this.subscriptionConnectionLifetime && this.subscriptionConnectionLifetime.closed;
-    }
-
-  }
-  return ConnectionInner;
+    return ConnectionInner;
 }
-
