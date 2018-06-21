4984dc3102d260e593d95ee61d7a08852506e34f
Cormac Relf <cormacrelf@me.com>
2018-06-21 11:45:47 +1000

    fix #4: TS < 2.7 compatibility by dropping implied unique symbol

--- packages/angular-skyhook/src/tokens.ts
+++ packages/angular-skyhook/src/tokens.ts
@@ -7,13 +7,19 @@ import { DragDropManager, Backend } from 'dnd-core';
 import { InjectionToken } from '@angular/core';
 
 /** The injection token for the dnd-core compatible backend currently in use. */
-export const DRAG_DROP_BACKEND = new InjectionToken<Backend>('dnd-core compatible backend');
+export const DRAG_DROP_BACKEND = new InjectionToken<Backend>(
+    'dnd-core compatible backend'
+);
 
 /** The injection token for the dnd-core DragDropManager */
-export const DRAG_DROP_MANAGER = new InjectionToken<DragDropManager<any>>('dnd-core DragDropManager');
+export const DRAG_DROP_MANAGER = new InjectionToken<DragDropManager<any>>(
+    'dnd-core DragDropManager'
+);
 
 /** The type a source or target is given as a marker for 'you supplied null as a type',
  *  so that library consumers can be reminded to use setType/setTypes manually.
  *  See {@link DragSource#setType}, {@link DropTarget#setTypes}.
  */
-export const TYPE_DYNAMIC = Symbol('no type specified, you must provide one with setType/setTypes');
+export const TYPE_DYNAMIC: symbol = Symbol(
+    'no type specified, you must provide one with setType/setTypes'
+);
