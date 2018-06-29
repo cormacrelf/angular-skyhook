606d94f322f4221ff5aad34c42f1288140da349b
Cormac Relf <web@cormacrelf.net>
2018-06-29 16:30:46 +1000

    fix(multi-backend): typings for touch backend now include all the options; use dnd-core types

--- packages/angular-skyhook-multi-backend/src/ambient.d.ts
+++ packages/angular-skyhook-multi-backend/src/ambient.d.ts
@@ -1,16 +1,19 @@
 declare module "dnd-multi-backend" {
+    import { Backend } from 'dnd-core';
+    import { BackendFactory } from 'dnd-core';
+    import { DragDropManager } from 'dnd-core';
     export interface Transition {
         event: string;
         check: (event: Event) => boolean;
     }
-    export type Backend = any;
     export type BackendTransition = {
-        backend: any;
+        backend: BackendFactory;
         transition: Transition;
+        preview?: boolean;
     };
     const MultiBackend: (
         transition: { backends: BackendTransition[] }
-    ) => Backend;
+    ) => BackendFactory;
     export default MultiBackend;
     export const createTransition: (
         event: string,
@@ -22,7 +25,21 @@ declare module "dnd-multi-backend" {
 }
 
 declare module "react-dnd-touch-backend" {
-    import { Backend } from "dnd-multi-backend";
-    const TouchBackend: ({ enableMouseEvents }: { enableMouseEvents: boolean }) => Backend;
+    import { Backend, BackendFactory } from "dnd-core";
+    export interface AngleRange {
+        start: number, // degrees, moving clockwise with 0/360 = pointing left
+        end: number, // degrees, moving clockwise with 0/360 = pointing left
+    }
+    export interface TouchBackendOptions {
+        enableMouseEvents?: boolean;
+        enableTouchEvents?: boolean,
+        enableKeyboardEvents?: boolean,
+        ignoreContextMenu?: boolean,
+        delayTouchStart?: number, // ms
+        delayMouseStart?: number, // ms
+        touchSlop?: number, // px
+        scrollAngleRanges?: AngleRange[] | undefined,
+    }
+    const TouchBackend: (options: TouchBackendOptions) => BackendFactory;
     export default TouchBackend;
 }
