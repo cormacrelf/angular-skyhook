d3f6d0c6d799a9ed7e1976587af9b498efb995e1
Cormac Relf <web@cormacrelf.net>
2018-07-05 03:46:11 +1000

    card-list: fixup last commit, ditch the placeholder directive

--- packages/angular-skyhook-card-list/src/module.ts
+++ packages/angular-skyhook-card-list/src/module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CardListComponent } from "./card-list.component";
 import { CardTemplateDirective } from "./card-template.directive";
-import { PlaceholderTemplateDirective } from "./card-placeholder.directive";
 import { CardRendererDirective } from "./card-renderer.directive";
 import { CommonModule } from "@angular/common";
 import { SkyhookDndModule } from "angular-skyhook";
 import { NgLetDirective } from "./ngLet.directive";
-import { PlaceholderDirective } from "./placeholder.directive";
 
 @NgModule({
     declarations: [
         CardListComponent,
         CardTemplateDirective,
         CardRendererDirective,
-        PlaceholderTemplateDirective,
-        PlaceholderDirective,
         NgLetDirective
     ],
     exports: [
         CardListComponent,
         CardTemplateDirective,
         CardRendererDirective,
-        PlaceholderTemplateDirective,
-        PlaceholderDirective,
     ],
     imports: [CommonModule, SkyhookDndModule]
 })
