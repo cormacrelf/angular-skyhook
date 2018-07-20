6f4184d6c54da0cc0fde352d88fae69e957aaa78
Cormac Relf <web@cormacrelf.net>
2018-07-20 16:05:59 +1000

    travis: -gk +dependabot; faster PR builds.

--- build-docs.sh
+++ build-docs.sh
@@ -1,55 +1,71 @@
+#!/bin/sh
+
 pkg="angular-skyhook"
 
 usage () {
   echo "usage: $0 [--serve] [--serve-only] [--no-examples] [--port <default is 8080>]"
 }
 
+fail () {
+    echo "failed on: $@"
+    echo "exiting"
+    exit 1
+}
+
 SERVE=0
 SERVE_ONLY=0
 EXAMPLES=1
-THEME=1
-TRAVIS=0
 PORT=8080
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -h | --help)
-            usage
-            exit
-            ;;
-        --travis)
-            TRAVIS=1
-            SERVE=0
-            SERVE_ONLY=0
-            EXAMPLES=1
-            THEME=1
-            break
-            ;;
-        --serve)
-            SERVE=1
-            ;;
-        --serve-only)
-            SERVE_ONLY=1
-            ;;
-        --no-examples)
-            EXAMPLES=0
-            ;;
-        --no-theme)
-            THEME=0
-            ;;
-        --port)
-            PORT=$2
-            shift
-            ;;
-        *)
-            echo "ERROR: unknown parameter \"$1\""
-            usage
-            exit 1
-            ;;
-    esac
-    shift
-done
+if [ -n "$TRAVIS" ]; then
+    SERVE=0
+    SERVE_ONLY=0
+    EXAMPLES=1
+else
+    while [ "$1" != "" ]; do
+        case $1 in
+            -h | --help)
+                usage
+                exit
+                ;;
+            --serve)
+                SERVE=1
+                ;;
+            --serve-only)
+                SERVE_ONLY=1
+                ;;
+            --no-examples)
+                EXAMPLES=0
+                ;;
+            --port)
+                PORT=$2
+                shift
+                ;;
+            *)
+                echo "ERROR: unknown parameter \"$1\""
+                usage
+                exit 1
+                ;;
+        esac
+        shift
+    done
+fi
 
+DIR=$(dirname "$0")
+output="$DIR/out-docs"
+skyhook="$DIR/packages/angular-skyhook"
+multi_backend="$DIR/packages/angular-skyhook-multi-backend"
+examples="$DIR/packages/examples"
+
+# Now, if we're running travis, we only want to build docs on master proper.
+# Anything less (e.g. PRs to master) and we can go faster by building in dev mode
+# and ignoring the docs (which never fail basically).
+# This saves about 1-2 minutes per non-master build.
+
+if [ "$TRAVIS_BRANCH" != "master" ] || [ -n "$TRAVIS_PULL_REQUEST" ]; then
+    (cd "$examples" && yarn run fast) || fail "build examples"
+    exit
+fi
 
 EXAMPLES_TASK="local-docs"
 if [[ $TRAVIS == 1 ]]; then
@@ -61,18 +77,6 @@ if [[ $SERVE_ONLY == "1" ]]; then
     (cd ./out-docs && python3 -m http.server $PORT)
     exit
 fi
-
-fail () {
-    echo "failed on: $@"
-    echo "exiting"
-    exit 1
-}
-
-DIR=$(dirname "$0")
-output="$DIR/out-docs"
-skyhook="$DIR/packages/angular-skyhook"
-multi_backend="$DIR/packages/angular-skyhook-multi-backend"
-examples="$DIR/packages/examples"
 
 yarn || fail "yarn install"
 
