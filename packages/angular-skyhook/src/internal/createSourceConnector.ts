405e7c4babe660ebbfaa715910ffb06e21f319fb
Cormac Relf <web@cormacrelf.net>
2018-07-20 12:09:35 +1000

    refactor: connectors now classes

--- packages/angular-skyhook/src/internal/createSourceConnector.ts
+++ packages/angular-skyhook/src/internal/createSourceConnector.ts
@@ -1,95 +1,92 @@
-/**
- * @ignore
- */
-/** a second comment */
-
 import { Backend, Unsubscribe } from 'dnd-core';
 import areOptionsEqual from '../utils/areOptionsEqual';
 import { DragSourceConnector } from '../connectors';
 
+export class SourceConnector {
+    private currentHandlerId: any;
+
+    private currentDragSourceNode: any;
+    private currentDragSourceOptions: any;
+    private disconnectCurrentDragSource: Unsubscribe | undefined;
+
+    private currentDragPreviewNode: any;
+    private currentDragPreviewOptions: any;
+    private disconnectCurrentDragPreview: Unsubscribe | undefined;
+
+    constructor(private backend: Backend) {
+    }
+
+    private reconnectDragSource() {
+        if (this.disconnectCurrentDragSource) {
+            this.disconnectCurrentDragSource();
+            this.disconnectCurrentDragSource = null;
+        }
+
+        if (this.currentHandlerId && this.currentDragSourceNode) {
+            this.disconnectCurrentDragSource = this.backend.connectDragSource(
+                this.currentHandlerId,
+                this.currentDragSourceNode,
+                this.currentDragSourceOptions,
+            );
+        }
+    }
+
+    private reconnectDragPreview() {
+        if (this.disconnectCurrentDragPreview) {
+            this.disconnectCurrentDragPreview();
+            this.disconnectCurrentDragPreview = null;
+        }
+
+        if (this.currentHandlerId && this.currentDragPreviewNode) {
+            this.disconnectCurrentDragPreview = this.backend.connectDragPreview(
+                this.currentHandlerId,
+                this.currentDragPreviewNode,
+                this.currentDragPreviewOptions,
+            );
+        }
+    }
+
+    public receiveHandlerId(handlerId: any) {
+        if (handlerId === this.currentHandlerId) {
+            return;
+        }
+
+        this.currentHandlerId = handlerId;
+        this.reconnectDragSource();
+        this.reconnectDragPreview();
+    }
+
+    public hooks: DragSourceConnector = {
+        dragSource: (nativeElement: any, options?: any) => {
+            if (
+                nativeElement === this.currentDragSourceNode &&
+                areOptionsEqual(options, this.currentDragSourceOptions)
+            ) {
+                return;
+            }
+
+            this.currentDragSourceNode = nativeElement;
+            this.currentDragSourceOptions = options;
+
+            this.reconnectDragSource();
+        },
+
+        dragPreview: (nativeElement: any, options?: any) => {
+            if (
+                nativeElement === this.currentDragPreviewNode &&
+                areOptionsEqual(options, this.currentDragPreviewOptions)
+            ) {
+                return;
+            }
+
+            this.currentDragPreviewNode = nativeElement;
+            this.currentDragPreviewOptions = options;
+
+            this.reconnectDragPreview();
+        },
+    };
+}
+
 export default function createSourceConnector(backend: Backend) {
-  let currentHandlerId: any;
-
-  let currentDragSourceNode: any;
-  let currentDragSourceOptions: any;
-  let disconnectCurrentDragSource: Unsubscribe | undefined;
-
-  let currentDragPreviewNode: any;
-  let currentDragPreviewOptions: any;
-  let disconnectCurrentDragPreview: Unsubscribe | undefined;
-
-  function reconnectDragSource() {
-    if (disconnectCurrentDragSource) {
-      disconnectCurrentDragSource();
-      disconnectCurrentDragSource = null;
-    }
-
-    if (currentHandlerId && currentDragSourceNode) {
-      disconnectCurrentDragSource = backend.connectDragSource(
-        currentHandlerId,
-        currentDragSourceNode,
-        currentDragSourceOptions,
-      );
-    }
-  }
-
-  function reconnectDragPreview() {
-    if (disconnectCurrentDragPreview) {
-      disconnectCurrentDragPreview();
-      disconnectCurrentDragPreview = null;
-    }
-
-    if (currentHandlerId && currentDragPreviewNode) {
-      disconnectCurrentDragPreview = backend.connectDragPreview(
-        currentHandlerId,
-        currentDragPreviewNode,
-        currentDragPreviewOptions,
-      );
-    }
-  }
-
-  function receiveHandlerId(handlerId: any) {
-    if (handlerId === currentHandlerId) {
-      return;
-    }
-
-    currentHandlerId = handlerId;
-    reconnectDragSource();
-    reconnectDragPreview();
-  }
-
-  const hooks: DragSourceConnector = {
-    dragSource: function connectDragSource(nativeElement: any, options?: any) {
-      if (
-        nativeElement === currentDragSourceNode &&
-        areOptionsEqual(options, currentDragSourceOptions)
-      ) {
-        return;
-      }
-
-      currentDragSourceNode = nativeElement;
-      currentDragSourceOptions = options;
-
-      reconnectDragSource();
-    },
-
-    dragPreview: function connectDragPreview(nativeElement: any, options?: any) {
-      if (
-        nativeElement === currentDragPreviewNode &&
-        areOptionsEqual(options, currentDragPreviewOptions)
-      ) {
-        return;
-      }
-
-      currentDragPreviewNode = nativeElement;
-      currentDragPreviewOptions = options;
-
-      reconnectDragPreview();
-    },
-  };
-
-  return {
-    receiveHandlerId,
-    hooks,
-  };
+    return new SourceConnector(backend);
 }
--- packages/angular-skyhook/src/internal/createTargetConnector.ts
+++ packages/angular-skyhook/src/internal/createTargetConnector.ts
@@ -1,61 +1,56 @@
-/**
- * @ignore
- */
-/** a second comment */
-
 import { Backend, Unsubscribe } from 'dnd-core';
 import areOptionsEqual from '../utils/areOptionsEqual';
 import { DropTargetConnector } from '../connectors';
 
+export class TargetConnector {
+    private currentHandlerId: any;
+    private currentDropTargetNode: any;
+    private currentDropTargetOptions: any;
+    private disconnectCurrentDropTarget: Unsubscribe | undefined;
+
+    constructor(private backend: Backend) {}
+
+    private reconnectDropTarget() {
+        if (this.disconnectCurrentDropTarget) {
+            this.disconnectCurrentDropTarget();
+            this.disconnectCurrentDropTarget = null;
+        }
+
+        if (this.currentHandlerId && this.currentDropTargetNode) {
+            this.disconnectCurrentDropTarget = this.backend.connectDropTarget(
+                this.currentHandlerId,
+                this.currentDropTargetNode,
+                this.currentDropTargetOptions,
+            );
+        }
+    }
+
+    public receiveHandlerId(handlerId: any) {
+        if (handlerId === this.currentHandlerId) {
+            return;
+        }
+
+        this.currentHandlerId = handlerId;
+        this.reconnectDropTarget();
+    }
+
+    public hooks: DropTargetConnector = {
+        dropTarget: (nativeElement: any, options?: any) => {
+            if (
+                nativeElement === this.currentDropTargetNode &&
+                areOptionsEqual(options, this.currentDropTargetOptions)
+            ) {
+                return;
+            }
+
+            this.currentDropTargetNode = nativeElement;
+            this.currentDropTargetOptions = options;
+
+            this.reconnectDropTarget();
+        },
+    };
+}
+
 export default function createTargetConnector(backend: Backend) {
-  let currentHandlerId: any;
-
-  let currentDropTargetNode: any;
-  let currentDropTargetOptions: any;
-  let disconnectCurrentDropTarget: Unsubscribe | undefined;
-
-  function reconnectDropTarget() {
-    if (disconnectCurrentDropTarget) {
-      disconnectCurrentDropTarget();
-      disconnectCurrentDropTarget = null;
-    }
-
-    if (currentHandlerId && currentDropTargetNode) {
-      disconnectCurrentDropTarget = backend.connectDropTarget(
-        currentHandlerId,
-        currentDropTargetNode,
-        currentDropTargetOptions,
-      );
-    }
-  }
-
-  function receiveHandlerId(handlerId: any) {
-    if (handlerId === currentHandlerId) {
-      return;
-    }
-
-    currentHandlerId = handlerId;
-    reconnectDropTarget();
-  }
-
-  const hooks: DropTargetConnector = {
-    dropTarget: function connectDropTarget(nativeElement: any, options?: any) {
-      if (
-        nativeElement === currentDropTargetNode &&
-        areOptionsEqual(options, currentDropTargetOptions)
-      ) {
-        return;
-      }
-
-      currentDropTargetNode = nativeElement;
-      currentDropTargetOptions = options;
-
-      reconnectDropTarget();
-    },
-  };
-
-  return {
-    receiveHandlerId,
-    hooks,
-  };
+    return new TargetConnector(backend);
 }
