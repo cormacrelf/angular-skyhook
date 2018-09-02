f9825f2166404a128903e9bf7bb80f4b34fedf74
Cormac Relf <web@cormacrelf.net>
2018-09-02 21:23:24 +1000

    build: lerna monkey-match to reflect https://github.com/alan-agius4/ng-mono-repo-starter/pull/69

--- tools/tasks/release.ts
+++ tools/tasks/release.ts
@@ -3,6 +3,7 @@ import * as lernaNpmPublish from "@lerna/npm-publish";
 import { join, resolve } from "path";
 import { execSync } from "child_process";
 import { readJsonSync, writeJsonSync } from "fs-extra";
+const log = require('npmlog');
 
 interface PackageJson {
 	name?: string;
@@ -41,44 +42,55 @@ try {
 	process.exit(1);
 }
 
-const log = require('npmlog');
-
+// Lerna's maintainers have refused to allow publishing from a subdirectory,
+// so we are going to monkey-patch the helper library that does this
+//
 // https://github.com/lerna/lerna/issues/91
+
 type Pkg = PackageJson & { location: string, rootPath: string; tarball: any };
+
 const originalNpmPack = (lernaNpmPublish as any).npmPack;
-const npmPack = (
-	rootManifest: any,
-	packages: Pkg[]
-) => {
+
+const npmPack = (rootManifest: any, packages: Pkg[]) => {
 	// modify the packages array in place
 	// because pkg.location is readonly so we can't modify each pkg
+	// and the packages array needs to be shared between the two calls into the npm-publish helper library:
+	// - first, npmPack, which attaches pkg.tarball = $(npm pack --json pkg.location) to each
+	// - second, npmPublish, which reads the same (===) array and expects to find pkg.tarball.filename,
+	//	 and runs `npm publish ${pkg.tarball.filename}`
+	// it is a little bad that the lib depends on array mutations happening between otherwise unrelated calls, but what can you do
 	for (let i = 0; i < packages.length; i++) {
 		let pkg = packages[i];
 		updateDistPackageJson(pkg.location);
 		const joined = join(pkg.location, "dist");
-		log.info('INTERCEPTED', 'publishing in', joined, 'instead');
+		// you could use log output to make a test suite for the monkey-patching
+		log.info('intercepted @lerna/npm-publish ', 'publishing', pkg.name, 'from', joined, 'instead');
 		packages[i] = {
 			...pkg,
 			location: joined,
 			// and also there is some weirdness with ...pkg not including all properties
+			// this solves it for whatever reason
 			rootPath: pkg.rootPath,
 			version: pkg.version,
 		};
 	}
-	return originalNpmPack(rootManifest, packages);
+	return originalNpmPack(rootManifest, packages /* opts -> gets default value */);
 };
 
-
+// somewhat reimplement https://github.com/lerna/lerna/blob/master/utils/npm-publish/npm-publish.js
 const modulePath = resolve("./node_modules/@lerna/npm-publish/npm-publish.js");
+// just monkey-patching npmPack is not enough or even useful -- that API is not used directly,
+// it is only used through makePacker
 require("module")._cache[modulePath].exports.npmPack = npmPack;
-// reimplement https://github.com/lerna/lerna/blob/master/utils/npm-publish/npm-publish.js
-// using our own npmPack
-require("module")._cache[modulePath].exports.makePacker =
-	(rootManifest: any) => {
-		// no opts will cause originalNpmPack to create opts = makePackOptions
-		return (packages: Pkg[]) => npmPack(rootManifest, packages);
-	};
+require("module")._cache[modulePath].exports.makePacker = (rootManifest: any) => {
+	// original redundantly specifies opts here
+	// passing no opts will cause originalNpmPack to create opts = makePackOptions(rootManifest)
+	// which is what we want
+	return (packages: Pkg[]) => npmPack(rootManifest, packages);
+};
 
+// strip '/path/to/node/binary'  and  './tools/tasks/release.js'
+// replace with 'publish'
 process.argv.splice(0, 2, "publish");
 
 const publishCmd = require("@lerna/publish/command");
